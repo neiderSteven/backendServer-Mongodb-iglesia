d379578247e09c3a1cf840f2b1b956e9ccf6f2da
neider steven rodrigue <neider.rodriguez@unac.edu.co>
2019-11-27 09:14:57 -0500

    actualizacion backend

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //cors
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+app.use(function (req, res, next) {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    /*res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Contro-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+    res.header("Access-Contro-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS")*/
     next();
-  });
+});
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: true }))
